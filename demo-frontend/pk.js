898bf6513f9e8c03ca86c0d3f47c4d22ea13fc05
Pravin-Sonwane-2004 <pravinson222@gmail.com>
2026-03-23 20:33:45 +0530

    deep diving inn js

--- demo-frontend/pk.js
+++ demo-frontend/pk.js
@@ -1,16 +1,42 @@
-const getUser = document.getElementById("btn");
-const user = document.getElementById("user");
+const btn = document.getElementById("btn");
 const div = document.getElementById("main-div");
+const deletebtn = document.getElementById("deletebtn");
 
-async function fetchData() {
-  const data = await fetch("http://localhost:8080/users");
-  console.log(data);
-  const actual = await data.json();
+ async function fetchData() {
+    const data = await fetch("http://localhost:8080/users");
+    const actual = await data.json();
     console.log(actual);
-}
+    return actual; // IMPORTANT
+  }
 
-btn.addEventListner ("click" , () => {
-const divv = document.createElement("newDiv");
-divv.innerText = "All";
-div.appendChild(div);
-}) ;
+  btn.addEventListener("click", async () => {
+    const users = await fetchData(); // WAIT for data
+    div.innerHTML = "";   //this will clear previous one
+      users.forEach(user => {
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+      <p>${user.id}</p>
+      <p>${user.name}</p>
+      <p>${user.email}</p>
+      
+    `;
+       // 🔥 create delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    // 🔥 attach event to THIS button
+    deleteBtn.addEventListener("click", () => {
+      userDiv.remove();
+    });
+
+    userDiv.appendChild(deleteBtn);
+
+    userDiv.style.border = "1px solid black";
+    userDiv.style.margin = "5px";
+    userDiv.style.padding = "5px";
+    userDiv.style.backgroundColor = "pink";
+
+    div.appendChild(userDiv);
+         
+      });
+  });
---
+++ demo-frontend/test.js
@@ -0,0 +1,23 @@
+const users = [
+  { id: 1, name: "A", active: true },
+  { id: 2, name: "B", active: false },
+  { id: 3, name: "C", active: true }
+];
+  let newArray = [];
+
+for(let i =0;i<users.length;i++) {
+    if(users[i].active) {
+        newArray[i] =  users[i];
+    }
+}
+console.log(newArray);
+
+for(let i =0;i<users.length;i++) {
+    if(users[i].active) {
+        newArray[i] =  users[i];
+        console.log(newArray[i].name);
+    }
+}
+//Count active users (using reduce)// i can easily use counter but i dont know what is reducer is
+
+
